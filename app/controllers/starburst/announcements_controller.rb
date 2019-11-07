1cdbd8be43e46e5896997002867711aa9da2bc8f
Jon Cockle <yongfook@gmail.com>
2019-11-07 20:50:42 +0700

    use hashid instead of id

--- app/controllers/starburst/announcements_controller.rb
+++ app/controllers/starburst/announcements_controller.rb
@@ -3,7 +3,7 @@ require_dependency 'starburst/application_controller'
 module Starburst
 	class AnnouncementsController < ApplicationController
 		def mark_as_read
-			announcement = Announcement.find(params[:id].to_i)
+			announcement = Announcement.with_hashids(params[:id]).first
 			if respond_to?(Starburst.current_user_method) && send(Starburst.current_user_method) && announcement
 				if AnnouncementView.where(user_id: send(Starburst.current_user_method).id, announcement_id: announcement.id).first_or_create(user_id: send(Starburst.current_user_method).id, announcement_id: announcement.id)
 					render :json => :ok
